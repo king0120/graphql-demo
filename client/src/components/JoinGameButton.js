e8f969604eaf26e789721c0d3e091451f5183a3d
king0120 <king0120@gmail.com>
2018-05-24 10:14:18 -0400

    subscriptions working

---
+++ client/src/components/JoinGameButton.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { gql } from 'graphql-tag'
+
+const JOIN_GAME = gql`
+    mutation startNewGame {
+        joinGame(playerName: "jim", gameId: "5b05c8f1bcb4373828572867"){
+            _id
+            players {
+                name
+            }
+        }
+    }
+`
+
+const JoinGameButton = () => {
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default JoinGameButton
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
-import ApolloClient from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import ApolloClient from 'apollo-client'
+import { HttpLink } from 'apollo-link-http'
+import { ApolloLink, concat, split } from 'apollo-link'
 import { ApolloProvider } from 'react-apollo'
-import gql from 'graphql-tag'
+import { getMainDefinition } from 'apollo-utilities'
+
 
 import { WebSocketLink } from 'apollo-link-ws'
+
+const httpLink = new HttpLink({
+  uri: 'http://localhost:3001/graphql'
+})
 
 const wsLink = new WebSocketLink({
   uri: `ws://localhost:3001/subscriptions`,
@@ -16,13 +24,28 @@ const wsLink = new WebSocketLink({
   }
 })
 
+const link = split(
+
+  // split based on operation type
+  ({ query }) => {
+    console.log(query)
+    const { kind, operation } = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  httpLink
+)
+
+
 const client = new ApolloClient({
-  link: wsLink,
-  uri: 'http://localhost:3001/graphql'
+  link,
+  cache: new InMemoryCache()
 })
 
+console.log(client)
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
-  </ApolloProvider>, document.getElementById('root'))
+  </ApolloProvider>, document.getElementById('root')
+)
 registerServiceWorker()
